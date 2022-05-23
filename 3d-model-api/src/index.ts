a3346e9b939057f961764b23ab6c442b4de9a0e0
Jovana Spasić <87784195+J-Spasic@users.noreply.github.com>
2022-05-23 11:20:21 +0200

    Enable usage of static assets

--- 3d-model-api/src/index.ts
+++ 3d-model-api/src/index.ts
@@ -4,27 +4,26 @@ import fs from 'fs';
 
 const app: Express = express();
 const PORT: number = parseInt(process.env.PORT as string, 10) || 8080;
+const assetsDirectoryPath: string = `${__dirname.substring(0, __dirname.length - 4)}assets`;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(assetsDirectoryPath));
 
 app.get('/3dModels', async (req: Request, res: Response): Promise<void> => {
-    const rootPathLength: number = __dirname.length - 4;
-    const assetsDirectoryPath: string = `${__dirname.substring(0, rootPathLength)}assets`;
-
     fs.readdir(assetsDirectoryPath, (err: NodeJS.ErrnoException | null, files: string[]): void => {
         if (err) {
             console.log(`Unable to scan directory: ${err}`);
         } else {
-            const arrayOfModels: Array<string> = new Array<string>();
+            const availableModels: string[] = [];
 
             files.forEach((file: string) => {
                 if (fs.statSync(`${assetsDirectoryPath}/${file}`).isDirectory()) {
-                    arrayOfModels.push(file);
+                    availableModels.push(file);
                 }
             });
 
-            res.send(arrayOfModels);
+            res.send(availableModels);
         }
     });
 });
@@ -37,7 +36,6 @@ app.get('/3dModels/:modelName', async (req: Request, res: Response): Promise<voi
     const modelName: string = req.params.modelName;
 
     const resourceUri: string = `${protocol}://${host}:${port}/assets/${modelName}/scene.gltf`;
-
     res.send(resourceUri);
 });
 
